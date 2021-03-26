a644cb2e3243edc72a77f4eeac03916cd2cb5da2
VerstraeteP <49196616+VerstraeteP@users.noreply.github.com>
2021-03-26 15:47:30 +0100

    Update stitch.py

--- stitch.py
+++ stitch.py
@@ -185,7 +185,7 @@ def stitching(images,masks):
 		matches = sorted(matches, key = lambda x:x.distance)
 		filtered_matches=matches[:200]
 		base_features=[base_features[m.queryIdx].pt for m in filtered_matches]
-		next_descs=[next_descs[m.queryIdx] for m in filtered_matches]
+		next_descs=[next_descs[m.trainIdx] for m in filtered_matches]
 		base_features = ssc(base_features, 20, 0.1, base_gray.shape[1], base_gray.shape[0])
 		base_features, base_desc= detector.compute(base_gray,base_features)
 		img3 = cv2.drawKeypoints(base_gray, base_features,base_gray, color=(255, 0, 0))
