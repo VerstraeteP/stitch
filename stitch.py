0f5b0261ef777d0fed2e160b06d9238daf75c221
VerstraeteP <49196616+VerstraeteP@users.noreply.github.com>
2021-04-01 13:12:31 +0200

    Update stitch.py

--- stitch.py
+++ stitch.py
@@ -298,7 +298,7 @@ def stitching(images,masks):
 			mod_photo = cv2.warpAffine(curr, transformation, (widthc, heightc))
 			base_msk = cv2.warpAffine(base_msk, transformation, (widthc, heightc))
 			mask_photo = cv2.warpAffine(base_mask, transformation, (widthc, heightc))
-      base_mask=cv2.warpAffine(base_mask, transformation, (widthc, heightc))
+      			base_mask=cv2.warpAffine(base_mask, transformation, (widthc, heightc))
 
 
 			
