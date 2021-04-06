b81a5f7f2692ebc1083eeb971afb08d0d3c8c61d
VerstraeteP <49196616+VerstraeteP@users.noreply.github.com>
2021-04-06 09:06:54 +0200

    Update stitch.py

--- stitch.py
+++ stitch.py
@@ -205,12 +205,12 @@ def stitching(images,masks):
 		#base_features, base_descs = detector.detectAndCompute(base_gray,mask_photo)
 		
 			
-		#base_features,base_descs=detector.detectAndCompute(base_gray,mask_photo)
-		base_features=goodFeaturesToTrack(base_gray, mask=mask_photo,minDistance=10)
-		base_features,base_descs=detector.compute(base_gray,base_features)
-		next_features=goodFeaturesToTrack(curr, mask=base_mask,minDistance=10)
-		next_features,base_descs=detector.compute(curr,next_features)
-		#next_features, next_descs = detector.detectAndCompute(curr,(base_mask))
+		base_features,base_descs=detector.detectAndCompute(base_gray,mask_photo)
+		#base_features=goodFeaturesToTrack(base_gray, mask=mask_photo,minDistance=10)
+		#base_features,base_descs=detector.compute(base_gray,base_features)
+		#next_features=goodFeaturesToTrack(curr, mask=base_mask,minDistance=10)
+		#next_features,base_descs=detector.compute(curr,next_features)
+		next_features, next_descs = detector.detectAndCompute(curr,(base_mask))
 		
 
 		bf = cv2.BFMatcher(cv2.NORM_HAMMING, crossCheck=True)
