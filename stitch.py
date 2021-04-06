1e1045405de9f2b078878708dc773e3fa3e2a126
VerstraeteP <49196616+VerstraeteP@users.noreply.github.com>
2021-04-06 09:12:04 +0200

    Update stitch.py

--- stitch.py
+++ stitch.py
@@ -408,8 +408,8 @@ def stitching(images,masks):
 			base_mask=cv2.warpAffine(base_mask, transformation, (widthc, heightc),flags=flag)
 
 		
-		img3 = cv2.drawMatches(base_gray,base_features,mod_photo,next_features,filtered_matches,None,flags=cv2.DrawMatchesFlags_NOT_DRAW_SINGLE_POINTS)		
-		cv2.imwrite("image"+str(times)+".jpg",img3)
+			img3 = cv2.drawMatches(base_gray,base_features,mod_photo,next_features,filtered_matches,None,flags=cv2.DrawMatchesFlags_NOT_DRAW_SINGLE_POINTS)		
+			cv2.imwrite("image"+str(times)+".jpg",img3)
 
 
 		if times>0:
