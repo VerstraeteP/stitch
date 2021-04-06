71e52f68c907d80a716e807f61926e5527fc540d
VerstraeteP <49196616+VerstraeteP@users.noreply.github.com>
2021-04-06 10:08:25 +0200

    Update stitch.py

--- stitch.py
+++ stitch.py
@@ -348,7 +348,7 @@ def stitching(images,masks):
 				dist=math.sqrt((src_pts[index][0]-dst_pts[index][0])**2+(src_pts[index][1]-dst_pts[index][1])**2)
 				sum+=dist
 				
-				print(dist)
+				
 				distance.append(dist)
 				
 				    
