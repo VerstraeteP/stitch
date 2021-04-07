a6bbf582799507a18169f3bf4b6e943d3a014941
VerstraeteP <49196616+VerstraeteP@users.noreply.github.com>
2021-04-07 11:05:32 +0200

    Update stitch.py

--- stitch.py
+++ stitch.py
@@ -117,7 +117,8 @@ def stitching(images,masks):
 		base_mask[:,:]=0
 		
 		curr[:,:]=0	
-		curr[start_img:cur_image.shape[0]+start_img,:cur_image.shape[1]]=cur_image
+		#curr[start_img:cur_image.shape[0]+start_img,:cur_image.shape[1]]=cur_image
+		curr[:cur_image.shape[0],:cur_image.shape[1]]=cur_image
 		"""
 		if len(base_msk.shape)==3:
 			base_msk=  cv2.cvtColor(base_msk, cv2.COLOR_BGR2GRAY)
