601c49bff015e1dc7883ebc83c4d51642b2a4a0c
VerstraeteP <49196616+VerstraeteP@users.noreply.github.com>
2021-04-07 13:16:07 +0200

    Update stitch.py

--- stitch.py
+++ stitch.py
@@ -318,7 +318,7 @@ def stitching(images,masks):
 		cv2.imwrite("before"+str(times)+".jpg",output)
 
 		
-		transformation, status = cv2.estimateAffine3D(dst_pts, src_pts,ransacReprojThreshold=10,maxIters=1000000 ,refineIters=1000000)
+		transformation, status = cv2.estimateAffine3D(dst_pts, src_pts)
 		#base_features=[]
 		#next_features=[]
 		filtered_matche=[]
