8f7c2fdf0644e2b7ede6f4660cb788ac4ff5e2fe
VerstraeteP <49196616+VerstraeteP@users.noreply.github.com>
2021-04-08 14:54:29 +0200

    Update stitch2.py

--- stitch2.py
+++ stitch2.py
@@ -172,7 +172,7 @@ def stitching(images,masks):
 			for index,k in enumerate(src_pts):
 				dist+=math.sqrt((src_pts[index][0]-dst_pts[index][0])**2+(src_pts[index][1]-dst_pts[index][1])**2)
 			print(dist)
-			if dist<best or best==None:
+			if best==None or dist<best:
 				best=dist
 				best_transformation=transformation
 				number_of_best=times
