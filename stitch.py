7cf4f874f769bd06d253f026e85cd78126d65723
VerstraeteP <49196616+VerstraeteP@users.noreply.github.com>
2021-04-09 18:08:22 +0200

    Update stitch.py

--- stitch.py
+++ stitch.py
@@ -68,7 +68,7 @@ def prepare_data_and_stitch(images,fps,scalingfactor=5):
 
 
 def stitching(images,masks):
-	total_transformation=[[1 , 0 ,0],[0,1,0]]
+	total_transformation=np.array([[1 , 0 ,0],[0,1,0]])
 	evaluate=0
 	"""
 	Stitch given images together to one uniform image
