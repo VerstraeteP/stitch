00030d0d06af9617bab539b7c48788c0820792dc
VerstraeteP <49196616+VerstraeteP@users.noreply.github.com>
2021-04-10 16:41:57 +0200

    Update stitch.py

--- stitch.py
+++ stitch.py
@@ -248,7 +248,7 @@ def stitching(images,masks):
 		matches = bf.knnMatch(base_descs,next_descs,k=2)
 		filtered_matches=[]
 		for m,n in matches:
-    			if m.distance < 0.5*n.distance:
+    			if m.distance < 0.2*n.distance:
         			filtered_matches.append(m)
 		
 		filtered_matches = np.asarray(filtered_matches)		
