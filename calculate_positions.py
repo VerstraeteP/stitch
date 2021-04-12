599df8865c99772638c164765209fc4f228ee5a5
VerstraeteP <49196616+VerstraeteP@users.noreply.github.com>
2021-04-12 17:03:05 +0200

    Update calculate_positions.py

--- calculate_positions.py
+++ calculate_positions.py
@@ -35,7 +35,7 @@ def calculate_pos(renners,Affinetransform,aantalrenners,afbeelding,fps_scaled,fp
 				bnd_left= np.array([[[k[2],k[3]]]],dtype= "float32")
 				
 				if index>2:
-					total=np.vstack((total_transform[index-1],[0,0,1]))
+					total=np.vstack((total_transform[index-2],[0,0,1]))
 
 					l=cv2.perspectiveTransform(pts,total)
 					l=cv2.perspectiveTransform(l,transformation)
