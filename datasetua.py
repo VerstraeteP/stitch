c0dc6ef9ab3b99697cc3757797fa67da4603c6de
VerstraeteP <49196616+VerstraeteP@users.noreply.github.com>
2021-04-12 11:43:53 +0200

    Update datasetua.py

--- datasetua.py
+++ datasetua.py
@@ -11,7 +11,7 @@ from importlib import reload
 
 def main():
 	
-	for file in glob.glob("./drive/MyDrive/videos/2019/kort.mp4"):
+	for file in glob.glob("./drive/MyDrive/videos/2019/kort_1_p.mp4"):
 		
 			outfiles=[]
 			print(file)
