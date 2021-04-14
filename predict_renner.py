591a273355652a47cb1940893823150a9f12f0c4
VerstraeteP <49196616+VerstraeteP@users.noreply.github.com>
2021-04-14 21:50:52 +0200

    Update predict_renner.py

--- predict_renner.py
+++ predict_renner.py
@@ -65,29 +65,29 @@ for d in ["train", "val"]:
 	MetadataCatalog.get("renner_" + d).set(thing_classes=["renner"])
 	balloon_metadata = MetadataCatalog.get("renner_train")
 	#dataset_dicts = get_balloon_dicts("surface_img/train")
-	cfg = get_cfg()
-	cfg.MODEL.DEVICE='cpu'
+cfg = get_cfg()
+cfg.MODEL.DEVICE='cpu'
 
-	cfg.merge_from_file(model_zoo.get_config_file("COCO-InstanceSegmentation/mask_rcnn_R_50_FPN_3x.yaml"))
-	cfg.DATASETS.TRAIN = ("renner_train",)
-	cfg.DATASETS.TEST = ()
-	cfg.DATALOADER.NUM_WORKERS = 2
-	cfg.MODEL.WEIGHTS = model_zoo.get_checkpoint_url("COCO-InstanceSegmentation/mask_rcnn_R_50_FPN_3x.yaml")  # Let training initialize from model zoo
-	cfg.SOLVER.IMS_PER_BATCH = 1
-	cfg.SOLVER.BASE_LR = 0.0025 #0.00025 # pick a good LR
-	cfg.SOLVER.MAX_ITER = 1000    # 300 iterations seems good enough for this toy dataset; you will need to train longer for a practical dataset
-	cfg.MODEL.ROI_HEADS.BATCH_SIZE_PER_IMAGE = 512   # faster, and good enough for this toy dataset (default: 512)
-	cfg.MODEL.ROI_HEADS.NUM_CLASSES = 1  # only has one class (ballon)
-	cfg.OUTPUT_DIR="./drive/MyDrive/renner"
+cfg.merge_from_file(model_zoo.get_config_file("COCO-InstanceSegmentation/mask_rcnn_R_50_FPN_3x.yaml"))
+cfg.DATASETS.TRAIN = ("renner_train",)
+cfg.DATASETS.TEST = ()
+cfg.DATALOADER.NUM_WORKERS = 2
+cfg.MODEL.WEIGHTS = model_zoo.get_checkpoint_url("COCO-InstanceSegmentation/mask_rcnn_R_50_FPN_3x.yaml")  # Let training initialize from model zoo
+cfg.SOLVER.IMS_PER_BATCH = 1
+cfg.SOLVER.BASE_LR = 0.0025 #0.00025 # pick a good LR
+cfg.SOLVER.MAX_ITER = 1000    # 300 iterations seems good enough for this toy dataset; you will need to train longer for a practical dataset
+cfg.MODEL.ROI_HEADS.BATCH_SIZE_PER_IMAGE = 512   # faster, and good enough for this toy dataset (default: 512)
+cfg.MODEL.ROI_HEADS.NUM_CLASSES = 1  # only has one class (ballon)
+cfg.OUTPUT_DIR="./drive/MyDrive/renner"
 
 
-	# cfg already contains everything we've set previously. Now we changed it a little bit for inference:
-	cfg.MODEL.WEIGHTS = os.path.join(cfg.OUTPUT_DIR, "model_final.pth")  # path to the model we just trained
-	cfg.MODEL.ROI_HEADS.SCORE_THRESH_TEST = 0.3 #0.7  # set a custom testing threshold
-	predictor = DefaultPredictor(cfg)
-	#dataset_dicts = get_balloon_dicts("balloon/train")
-	dataset_dicts="surface_img/val"
-	files=sorted(glob.glob("balloon/val/*.jpg"))
+# cfg already contains everything we've set previously. Now we changed it a little bit for inference:
+cfg.MODEL.WEIGHTS = os.path.join(cfg.OUTPUT_DIR, "model_final.pth")  # path to the model we just trained
+cfg.MODEL.ROI_HEADS.SCORE_THRESH_TEST = 0.3 #0.7  # set a custom testing threshold
+predictor = DefaultPredictor(cfg)
+#dataset_dicts = get_balloon_dicts("balloon/train")
+dataset_dicts="surface_img/val"
+files=sorted(glob.glob("balloon/val/*.jpg"))
 	
 
 def predict_renner(images,masks):
