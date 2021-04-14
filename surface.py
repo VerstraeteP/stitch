1d8481172ca084583b8a63640bd6a65491259a88
VerstraeteP <49196616+VerstraeteP@users.noreply.github.com>
2021-04-14 15:39:35 +0200

    Update surface.py

--- surface.py
+++ surface.py
@@ -126,7 +126,7 @@ def predict_surface(img):
 		prev_x_min=0
 		prev_x_max=k.shape[1]
 		if len(outputs['instances'].pred_boxes)==0:
-			pass
+			maskoutput=np.zeros(k.shape[0],k.shape[1])
 		else:
 			for index,k in enumerate(outputs['instances'].pred_boxes.to("cpu")):
 				coordinates=k.numpy()
