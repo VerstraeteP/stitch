2eeee535e083ade2516090dc66a28cb221c5a695
VerstraeteP <49196616+VerstraeteP@users.noreply.github.com>
2021-04-19 13:54:48 +0200

    Update stitch.py

--- stitch.py
+++ stitch.py
@@ -13,7 +13,7 @@ from predict_renner import predict_renner
 
 import matplotlib.pyplot as plt
 
-def prepare_data_and_stitch(images,fps,scalingfactor=20):
+def prepare_data_and_stitch(images,fps,scalingfactor=40):
 	
 	"""
 	calculates the mask of the images
