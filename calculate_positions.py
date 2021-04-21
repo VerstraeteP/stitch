9d1d751056c4ca9cbb662fb152f31d7a5643be8a
VerstraeteP <49196616+VerstraeteP@users.noreply.github.com>
2021-04-21 14:44:49 +0200

    Update calculate_positions.py

--- calculate_positions.py
+++ calculate_positions.py
@@ -40,26 +40,26 @@ def calculate_pos(renners,Affinetransform,aantalrenners,afbeelding,fps_scaled,fp
 			prev[1][2]=0
 			
 			copy=Affinetransform[index].copy()
-			l=cv2.perspectiveTransform(line,total)
-			l=cv2.perspectiveTransform(l,prev)
-			l=cv2.perspectiveTransform(l,np.vstack((copy,[0,0,1])))
-			l[0][0][0]+=offset
-			l[0][1][0]+=offset
+			line=cv2.perspectiveTransform(line,total)
+			line=cv2.perspectiveTransform(line,prev)
+			line=cv2.perspectiveTransform(line,np.vstack((copy,[0,0,1])))
+			line[0][0][0]+=offset
+			line[0][1][0]+=offset
 			
 			
 		if index==0:
 		
 			line =np.array([[[line[0][0][0]+500,line[0][0][1]],[line[0][1][0]+500,line[0][1][1]]]], dtype = "float32")
-			l=cv2.perspectiveTransform(line,np.vstack((Affinetransform[index],[0,0,1])))
+			line=cv2.perspectiveTransform(line,np.vstack((Affinetransform[index],[0,0,1])))
 			
 		if index==1:
 		
 			line =np.array([[[line[0][0][0]+300,line[0][0][1]+300],[line[0][1][0]+300,line[0][1][1]+300]]], dtype = "float32")
 			copy=Affinetransform[index].copy()
 
-			l=cv2.perspectiveTransform(line,np.vstack((copy,[0,0,1])))
-			l[0][0][0]+=offset
-			l[0][1][0]+=offset
+			line=cv2.perspectiveTransform(line,np.vstack((copy,[0,0,1])))
+			line[0][0][0]+=offset
+			line[0][1][0]+=offset
 		
 		
 		linearray.append(l)
