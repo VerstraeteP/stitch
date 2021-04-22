2e165c76d247a5f4b7b3ca29fe5a3deff546a063
VerstraeteP <49196616+VerstraeteP@users.noreply.github.com>
2021-04-22 23:06:44 +0200

    Update datasetua.py

--- datasetua.py
+++ datasetua.py
@@ -11,7 +11,7 @@ from importlib import reload
 
 def main():
 	
-	for file in glob.glob("./drive/MyDrive/videos/2019/Rondevanfrankrijk_7_Chalon-sur-Saône.mp4"):
+	for file in glob.glob("./drive/MyDrive/videos/2019/*.mp4"):
 		try:
 			outfiles=[]
 			print(file)
@@ -40,5 +40,32 @@ def main():
 		except:
 			continue
 	
-		
+	for file in glob.glob("./drive/MyDrive/videos/2017/*.mp4"):
+		try:
+			outfiles=[]
+			print(file)
+			
+			path, wedstrijd = os.path.split(file)
+			path, jaartal = os.path.split(path)
+			filename=wedstrijd.split('.')[0]
+			wedstrijdnaam=wedstrijd.split('_')[0]
+			img=cv2.imread("jersey.png")
+			rit=wedstrijd.split('_')[1]
+			aankomstplaats=wedstrijd.split('_')[2].split('.')[0]
+			
+			jsonfinal={"Metadata":{"Name":wedstrijdnaam,"Year":jaartal,"Stage":rit,"City":aankomstplaats}}
+			stitch,line,renner,mask,transformaties=start(file)
+			
+			
+			cv2.imwrite("./drive/MyDrive/dataset/stitch/"+str(jaartal)+"/"+filename+".jpg",stitch)
+			cv2.imwrite("./drive/MyDrive/dataset/lines/"+str(jaartal)+"/"+filename+".jpg",line)
+			cv2.imwrite("./drive/MyDrive/dataset/mask/"+str(jaartal)+"/"+filename+".jpg",mask)
+			outfiles.append(jsonfinal)
+			outfiles.append(renner)
+			with open("./drive/MyDrive/dataset/json/"+str(jaartal)+"/"+filename+".txt", 'w') as outfile:
+				json.dump(outfiles, outfile)
+			with open("./drive/MyDrive/dataset/transformaties/"+str(jaartal)+"/"+filename+".txt", 'w') as outfile:
+				json.dump(transformaties, outfile)
+		except:
+			continue	
 main()
