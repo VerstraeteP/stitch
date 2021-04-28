8682e5ba72730aeef81d733e3f6914d67ba7dc61
VerstraeteP <49196616+VerstraeteP@users.noreply.github.com>
2021-04-28 08:56:40 +0200

    Update surface.py

--- surface.py
+++ surface.py
@@ -166,9 +166,9 @@ def predict_surface(img):
 				indexen=[best_ind]
 			print("10")
 
-			prev_x_min=coordinaten[best_ind][0]
-			prev_x_max=coordinaten[best_ind][2]
-			pritn("11")			     
+				prev_x_min=coordinaten[best_ind][0]
+				prev_x_max=coordinaten[best_ind][2]
+						     
 			
 		for index,k in enumerate(outputs['instances'].pred_masks.to("cpu").numpy()):
 			if indexen.count(index)==1:
