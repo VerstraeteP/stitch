7ee6a0ca570d5153b4514bcfaa0af5c42c7e5e14
VerstraeteP <49196616+VerstraeteP@users.noreply.github.com>
2021-04-28 08:50:21 +0200

    Update surface.py

--- surface.py
+++ surface.py
@@ -142,7 +142,7 @@ def predict_surface(img):
 					#prev_x_min=coordinates[0]
 					#prev_x_max=coordinates[2]
 
-
+			print("9")
 			best_ind=0
 			if len(indexen)>1:
 				best=None
@@ -164,6 +164,7 @@ def predict_surface(img):
 							     best_ind=d
 
 				indexen=[best_ind]
+			print("10")
 
 			prev_x_min=coordinaten[best_ind][0]
 			prev_x_max=coordinaten[best_ind][2]
