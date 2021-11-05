9bdaebd0ee0aead93de73af84e10cd0ef5c115c0
VerstraeteP <49196616+VerstraeteP@users.noreply.github.com>
2021-11-05 14:04:05 +0100

    Update datasetua.py

--- datasetua.py
+++ datasetua.py
@@ -1,6 +1,5 @@
 
 from main import start
-
 import os
 import glob
 import json
@@ -11,37 +10,37 @@ from importlib import reload
 
 def main():
 	
-	for file in glob.glob("./drive/MyDrive/videos/2021/GentWevelgem_1_dames.mp4"):
+	for file in glob.glob("./drive/MyDrive/videos/2021/GentWevelgem_1_dames.mp4"): 						#google drive folder with videos
 		
 			outfiles=[]
-			print(file)
-			
-			path, wedstrijd = os.path.split(file)
+			path, wedstrijd = os.path.split(file)							
 			path, jaartal = os.path.split(path)
 			filename=wedstrijd.split('.')[0]
 			wedstrijdnaam=wedstrijd.split('_')[0]
-			img=cv2.imread("jersey.png")
 			rit=wedstrijd.split('_')[1]
 			aankomstplaats=wedstrijd.split('_')[2].split('.')[0]
 			
-			jsonfinal={"Metadata":{"Name":wedstrijdnaam,"Year":jaartal,"Stage":rit,"City":aankomstplaats}}
-			stitch,line,renner,mask,transformaties,renners,transposition=start(file)
+			stitch,line,renner,mask,transformaties,renners,transposition=start(file)				#call start function from main.py 
+			
 			json_format = json.dumps(str(renners))
 			jsonformat = json.dumps(str(transposition))
-			with open("./drive/MyDrive/dataset/boundingbox/"+str(jaartal)+"/"+filename+".txt", 'w') as outfile:
-				json.dump(json_format, outfile)
-			with open("./drive/MyDrive/dataset/transposition/"+str(jaartal)+"/"+filename+".txt", 'w') as outfile:
-				json.dump(jsonformat, outfile)
-			
-			cv2.imwrite("./drive/MyDrive/dataset/stitch/"+str(jaartal)+"/"+filename+".jpg",stitch)
-			cv2.imwrite("./drive/MyDrive/dataset/lines/"+str(jaartal)+"/"+filename+".jpg",line)
-			cv2.imwrite("./drive/MyDrive/dataset/mask/"+str(jaartal)+"/"+filename+".jpg",mask)
-			outfiles.append(jsonfinal)
+			outfiles.append(jsonfinal)										#combine both metadata + riders positions
 			outfiles.append(renner)
-			with open("./drive/MyDrive/dataset/json/"+str(jaartal)+"/"+filename+".txt", 'w') as outfile:
+			jsonfinal={"Metadata":{"Name":wedstrijdnaam,"Year":jaartal,"Stage":rit,"City":aankomstplaats}} 		#metadata race info
+			
+			with open("./drive/MyDrive/dataset/boundingbox/"+str(jaartal)+"/"+filename+".txt", 'w') as outfile: 	#dump boundingboxes of detected riders
+				json.dump(json_format, outfile)
+			with open("./drive/MyDrive/dataset/transposition/"+str(jaartal)+"/"+filename+".txt", 'w') as outfile:	#dump transformed boundingboxes of detected riders
+				json.dump(jsonformat, outfile)
+			with open("./drive/MyDrive/dataset/json/"+str(jaartal)+"/"+filename+".txt", 'w') as outfile:		#dump metadata+riders position
 				json.dump(outfiles, outfile)
-			with open("./drive/MyDrive/dataset/transformaties/"+str(jaartal)+"/"+filename+".txt", 'w') as outfile:
+			with open("./drive/MyDrive/dataset/transformaties/"+str(jaartal)+"/"+filename+".txt", 'w') as outfile:	#dump transformation matrices
 				json.dump(transformaties, outfile)
+			
+			cv2.imwrite("./drive/MyDrive/dataset/stitch/"+str(jaartal)+"/"+filename+".jpg",stitch)  		#dump stitched image
+			cv2.imwrite("./drive/MyDrive/dataset/lines/"+str(jaartal)+"/"+filename+".jpg",line)			#dump stitched image+ riders lines 
+			
+			
 		
 		
 main()
