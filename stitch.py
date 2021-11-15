63b357befdf2db0c334e986778b4a3e1647f6ce8
VerstraeteP <49196616+VerstraeteP@users.noreply.github.com>
2021-11-15 13:45:50 +0100

    Update stitch.py

--- stitch.py
+++ stitch.py
@@ -89,6 +89,7 @@ def stitching(images,masks):
 	border=5
 	vergroot=0
 	indexen=[]									#list of indexen: list holds the position where the image is enlarged
+	array=np.array([0,0,1])
 	lengte=len(images)
 	for cur_image in images[1:]:								#go through all images starting from the second image
 		try:										#try-catch block: it can occur that the stitching fails,if it fails:try to restore the maximum successful image
@@ -136,12 +137,13 @@ def stitching(images,masks):
 					trans = np.vstack((tran,array))
 
 
-					total_transformation=np.dot(total_transformation,trans)
+					total_transformation=np.dot(total_transformation,trans)			#do a dot product of the total transformation and the last transformation, so we get a new total transformation
 					total_transformation=total_transformation[:2,:]
-					total_affine.append(total_transformation)
+					total_affine.append(total_transformation)				#add to the total tranformation list
 
 					transformation=transformation[:2,:]
-				#in the next paragraph: check if the result image needs to be enlarged, we take some threshold space needed to make sure the new image will fit in the total stitched image
+					
+				#in the next paragraph: check if the result image needs to be enlarged, we take some threshold space to make sure the new image will fit in the total stitched image
 				for k,i in enumerate(base_gray[baseline:,:]):					# check in y-direction, starting from baseline(last known position), go trough all lines
 					if(~i.any()):								#if in any line all values are = 0 occures: this is the last empty line 
 						baseline=k+baseline						#add number of line to baseline variable
@@ -167,50 +169,50 @@ def stitching(images,masks):
 
 
 				if (baselinex+int(cur_image.shape[1])/2)>base_gray.shape[1]:			#if detected edge + half of new image is smaller than current stitched image, than make stitched image larger
-					vergroot+=1
+					vergroot+=1								# we increase "vergroot" ( max 20times)
 
-					base_gray = np.append(base_gray,increase,axis=1)
+					base_gray = np.append(base_gray,increase,axis=1)			# in the following section we add our blank image to our existing images
 					total_mask = np.append(total_mask,increase_mask,axis=1)
 					mask_photo= np.append(mask_photo,increase_mask,axis=1)
 					base_mask= np.append(base_mask,increase_mask,axis=1)
 					curr = np.append(curr,increase,axis=1)
-					increasex=np.zeros((images[0].shape[0],curr.shape[1],3), np.uint8)
+					increasex=np.zeros((images[0].shape[0],curr.shape[1],3), np.uint8)	# we define new blank images, taking into account the new dimensions of our images, we need to 
 					increase_mask_x=np.zeros((images[0].shape[0],curr.shape[1]), np.uint8)
 					heightc, widthc = curr.shape[:2]
 
 
-				if (baseline+cur_image.shape[0])>=base_gray.shape[0]:
+				if (baseline+cur_image.shape[0])>=base_gray.shape[0]:		#again check if image is large enough 
 					vergroot+=1
 					mask_photo= np.append(mask_photo,increase_mask_x,axis=0)
 					base_gray = np.append(base_gray,increasex,axis=0)
 					base_mask= np.append(base_mask,increase_mask_x,axis=0)
 					total_mask = np.append(total_mask,increase_mask_x,axis=0)
 					curr = np.append(curr,increasex,axis=0)
-					increase=np.zeros((curr.shape[0],images[0].shape[1],3), np.uint8)
+					increase=np.zeros((curr.shape[0],images[0].shape[1],3), np.uint8)	# change blank images dimension 
 					increase_mask=np.zeros((curr.shape[0],images[0].shape[1]), np.uint8)
 					heightc, widthc = curr.shape[:2]
 
-				if (baselineneg-cur_image.shape[1]/2)<0:
+				if (baselineneg-cur_image.shape[1]/2)<0:					#check if stitched image, is comming close to left border: if this happens change image to the right, by adding extra pixel columns
 					vergroot+=1
 
-					indexen.append(teller)
+					indexen.append(teller)							#add the number of the image to index list, so we know where we shifted our image to the right, so we can adapt our rider positions
 					baselines.append(baselineneg)
-					base_gray = np.append(base_gray,increase,axis=1)
+					base_gray = np.append(base_gray,increase,axis=1)			# add blank image to image at the right side
 					total_mask = np.append(total_mask,increase_mask,axis=1)
 					mask_photo= np.append(mask_photo,increase_mask,axis=1)
 					base_mask= np.append(base_mask,increase_mask,axis=1)
 					curr = np.append(curr,increase,axis=1)
-					base_gray[:,baselineneg:]=base_gray[:,:base_gray.shape[1]-baselineneg]
+					base_gray[:,baselineneg:]=base_gray[:,:base_gray.shape[1]-baselineneg]	#shift every image to the right
 					curr[:,baselineneg:]=curr[:,:curr.shape[1]-baselineneg]
 					total_mask[:,baselineneg:]=total_mask[:,:total_mask.shape[1]-baselineneg]
 					base_mask[:,baselineneg:]=base_mask[:,:base_mask.shape[1]-baselineneg]
 					mask_photo[:,baselineneg:]=mask_photo[:,:mask_photo.shape[1]-baselineneg]
-					base_gray[:,:baselineneg]=0
+					base_gray[:,:baselineneg]=0						#set rest of pixels to 0
 					total_mask[:,:baselineneg]=0
 					base_mask[:,:baselineneg]=0
 					mask_photo[:,:baselineneg]=0
 					curr[:,:baselineneg]=0
-					increasex=np.zeros((images[0].shape[0],curr.shape[1],3), np.uint8)
+					increasex=np.zeros((images[0].shape[0],curr.shape[1],3), np.uint8)	#change the dimensions of our blank image
 					increase_mask_x=np.zeros((images[0].shape[0],curr.shape[1]), np.uint8)
 					heightc, widthc = curr.shape[:2]
 					baselineneg+=increase.shape[1]
@@ -257,7 +259,7 @@ def stitching(images,masks):
 
 				transformation, status = cv2.estimateAffine2D(dst_pts, src_pts,ransacReprojThreshold=5,maxIters=10000 ,refineIters=1000)	#with the src_pts and dst_pts we can find a transformation between those points, ransacReprojThreshold= maximum reprojection error in the RANSAC algorithm to consider a point as an inlier.
 				filtered_matche=[]
-				array=np.array([0,0,1])
+				
 
 				Affinetransformations.append(transformation)
 
@@ -269,7 +271,6 @@ def stitching(images,masks):
 				(ret,data_map) = cv2.threshold(cv2.cvtColor(mod_photo, cv2.COLOR_BGR2GRAY),0, 255,cv2.THRESH_BINARY)	# make a mask in the new image
 
 				contours, hierarchy = cv2.findContours(data_map, cv2.RETR_TREE, cv2.CHAIN_APPROX_SIMPLE)		#findcontour, otherwise, a black line in stitched image, we find contour and color it black, so mask is a little smaller
-				contours1, hierarchy1 = cv2.findContours(base_msk, cv2.RETR_TREE, cv2.CHAIN_APPROX_SIMPLE)
 				cv2.drawContours(data_map, contours, -1, (0,255,255), 10)
 				enlarged_base_img= cv2.bitwise_and(total_mask,total_mask, mask =np.bitwise_not(data_map))
 				enlarged_base_img1 = cv2.bitwise_and(base_gray,base_gray,mask =np.bitwise_not(data_map))
@@ -277,19 +278,19 @@ def stitching(images,masks):
 
 				mod_photo= cv2.bitwise_and(mod_photo,mod_photo,mask =(data_map))
 				mod_photo1= cv2.bitwise_and(base_msk,base_msk,mask =(base_msk))
-				final_img = cv2.add(mod_photo,enlarged_base_img1,dtype=cv2.CV_8U)
+				final_img = cv2.add(mod_photo,enlarged_base_img1,dtype=cv2.CV_8U)					#our final stitch image
 
 				
 				
 					
 				base_gray=final_img
 																	# every 5 frames we save a temporary image, so if it fails we can go back
-				if cnt==0:	
+				if cnt==0:												#if it's our first image, assign all variables
 					prev_base_gray= base_gray
 					prev_prev_base_gray=base_gray
 					total_transformation=transformation
 					total_transformation = np.vstack((total_transformation,array))
-				elif cnt%5==0:
+				elif cnt%5==0:												#else every 5 frames store image
 					prev_prev_base_gray= prev_base_gray
 					prev_base_gray=base_gray
 
@@ -311,7 +312,6 @@ def stitching(images,masks):
 
 				"""
 				total_mask= cv2.add(mod_photo1,enlarged_base_img,dtype=cv2.CV_8U)
-				cv2.imwrite("./drive/MyDrive/wkvideo/base_gray/"+str(teller)+".jpg",base_gray)
 				teller=teller+1
 				
 				
